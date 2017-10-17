b813ae5e3a74afb9c6c094b5e20289d5ec805e3a
genjo <insidia93@gmail.com>
2017-10-17 09:40:07 +0300

    Created productcontroller

---
+++ controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: genjo
+ * Date: 17.10.17
+ * Time: 8:21
+ */
+
+namespace app\controllers;
+
+
+use app\models\Products;
+
+class ProductController extends AppController
+{
+
+    public function actionView()
+    {
+        $id= \Yii::$app->request->get('id');
+        $product = Products::findOne($id);
+        $hits = Products::find()->where(['hit'=>'1'])->limit(6)->all();
+        $this->setMeta('E-SHOPPER | '.$product->name,$product->keywords,$product->description);
+        return $this->render('view',compact('product','hits'));
+    }
+}
